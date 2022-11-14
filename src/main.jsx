f0018183f29c53096b9da02eb08dcc86efe8c245
Troy Underwood <troyizzle@gmail.com>
2022-11-14 17:38:50 -0600

    add error element to react router

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,13 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Root from "./routes/root"
+import ErrorPage from "./error-page"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />
   },
 ]);
 
