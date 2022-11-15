2a2aeb0ed18bc413c7d6acfcf3ca21eed794192b
Troy Underwood <troyizzle@gmail.com>
2022-11-14 19:31:08 -0600

    update root searching

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -9,8 +9,10 @@ import {
 import { getContacts, createContact } from "../contacts";
 
 export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export async function action() {
@@ -19,7 +21,7 @@ export async function action() {
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
 
   return (
@@ -27,17 +29,18 @@ export default function Root() {
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
